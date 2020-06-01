bbab8c91923a19011bd28ec226ff89eebb5bf448
shusheng <863193681@qq.com>
2020-06-01 10:22:25 +0800

    '修改'

--- src/pages/StudioMoreHome.vue
+++ src/pages/StudioMoreHome.vue
@@ -10,30 +10,64 @@
     </template>
   </van-nav-bar>
   <div style="height: 46px"></div>
-  <van-tabs v-model="tabActive" animated @click="tabChange">
+  <van-tabs v-model="tabActive" title-active-color='#278bef' color="#278bef">
     <van-tab :title="'工作室联盟'">
-      <van-grid class="grid-wrap" :gutter="10">
-        <van-grid-item class="grid-item" v-for="(item, i) in studioUnionList" :key="i" :text="item.name" @click="studioUnion(item)" >
-          <img :src="item.img" alt="">
-          <p>{{item.name}}</p>
-          <div class="action-wrap">
-            <span v-if="item.isFollow == 0" class="unfollow">关注</span>
-            <span v-else class="followed">已关注</span>
-          </div>
-        </van-grid-item>
-      </van-grid>
+          <div class="choose"> 没有搜索结果 <span @click="choose">筛选 <van-icon name="arrow-down" /></span> </div>
+             <van-grid class="grid-wrap" :gutter="10">
+            <van-grid-item class="grid-item" v-for="(item, i) in studioUnionList" :key="i" :text="item.name" @click="studioUnion(item)" >
+            <img :src="item.img" alt="">
+            <p>{{item.name}}</p>
+            <div class="action-wrap">
+              <span v-if="item.isFollow == 0" class="unfollow">关注</span>
+              <span v-else class="followed">已关注</span>
+            </div>
+            </van-grid-item>
+        </van-grid>
     </van-tab>
     <van-tab :title="'星级工作室'">
-      <div class="tab">
-        煽动反共水电费
-      </div>
+       <div class="choose"><span @click="choose">筛选 <van-icon name="arrow-down" /></span> </div>
     </van-tab>
     <van-tab :title="'所在单位工作室'">
-      <div class="tab">
-        煽动反共水电费
-      </div>
+       <div class="choose"> <span @click="choose">筛选 <van-icon name="arrow-down" /></span> </div>
     </van-tab>
   </van-tabs>
+        <!-- 弹窗 -->
+     <van-popup v-model="show"  position="bottom"  :style="{ height: '80%' }">  
+      <div class="choose1">筛选</div>
+      <van-tabs v-model="tabActive1" animated  title-active-color='#278bef' color="#278bef" >
+       <van-tab :title="'所在单位'">
+          <van-collapse v-model="activeName" v-for="i of list" :key="i.id" accordion>
+           <van-collapse-item :name="i.id">
+             <template #title>
+               <van-checkbox
+                 class="checkbox"
+                 v-model="i.checked"
+                 @click.stop="handclick(i)"
+                 shape="square"
+                 label-disabled
+               ></van-checkbox>
+               {{i.name}}
+             </template>
+             <van-checkbox-group v-model="result" :ref="i.id" class="group" @change="handChangeclick(i)">
+               <div class="item" v-for="i1 of i.children" :key="i1.id">
+                 <van-checkbox :name="i1.id" shape="square" >{{i1.name}}</van-checkbox>
+               </div>
+             </van-checkbox-group>
+           </van-collapse-item>
+          </van-collapse>
+          </van-tab>
+          <van-tab :title="'工作室联盟'">
+            没有搜索结果 
+          </van-tab>
+          <van-tab :title="'所在星级'">
+            没有搜索结果 
+          </van-tab>
+        </van-tabs>  
+        <van-row class="btn">
+          <van-col span="12"><van-button type="default" size="large" @click="result=[]">重置</van-button></van-col>
+          <van-col span="12"><van-button type="info" size="large" @click="addStudioMore">确定</van-button></van-col>   
+        </van-row>
+    </van-popup>
 </div>
 </template>
 
@@ -49,7 +83,11 @@ export default {
   // },
   data () {
   return {
+    activeName: [],
+    show:false,
     tabActive: 0,
+    tabActive1: 0,
+    result: [],
     studioUnionList: [
       {name: 'A协会协会反光杯风骨霸刀服不服', isFollow: 0, img: require('../assets/img/photo.jpg')},
       {name: 'B协会联盟', isFollow: 1, img: require('../assets/img/photo.jpg')},
@@ -76,6 +114,46 @@ export default {
       {name: '所在单位工作室4', isFollow: 1, img: require('../assets/img/photo.jpg')},
       {name: '所在单位工作室5', isFollow: 0, img: require('../assets/img/photo.jpg')}
     ],
+     list: [
+        {
+          id: 1,
+          checked: false,
+          name: "广东电网有限公司",
+          children: [
+            {
+              id: 11,
+              name: "广州电网公司"
+            },
+            {
+              id: 12,
+              name: "深圳电网公司"
+            },
+            {
+              id: 13,
+              name: "珠海电网公司"
+            }
+          ]
+        },
+        {
+          id: 2,
+          checked: false,
+          name: "广东电网有限公司",
+          children: [
+            {
+              id: 21,
+              name: "广州电网公司"
+            },
+            {
+              id: 22,
+              name: "广州电网公司"
+            },
+            {
+              id: 23,
+              name: "广州电网公司"
+            }
+          ]
+        }
+      ]
   }
   },
   created () {
@@ -104,10 +182,7 @@ export default {
     allStudioUnion() {
       // this.$router.push("/photoAlbum");
     },
-    // 切换tab
-    tabChange() {
-      console.log(this.active);
-    },
+
     // 进入已关注/未关注工作室
     studioUnion(item) {
       this.$router.push(
@@ -120,17 +195,54 @@ export default {
         }
       )
     },
+    // 弹窗筛选框
+    choose(){
+      this.show=true
+    },
+     
+     //点击1级checkbox框
+    handclick(i) {
+      let newIndexs = i.children.map(val => val.id);
+      if (i.checked == false) {
+        this.result.push(...newIndexs.filter(el => this.result.indexOf(el) === -1));
+      } else {
+        this.result = this.result.filter(val => newIndexs.indexOf(val) === -1);
+      }
+    },
+
+    //点击确定按钮
+    addStudioMore(){
+      let msg=this.result.length!=0?this.result.join(','):'请选择关注内容'
+      Toast(msg);
+      this.show=false
+    },
+    //点击2级checkbox框
+    handChangeclick(i){
+      let newIndexs = i.children.map(val => val.id);
+     i.checked=newIndexs.find(el=>this.result.indexOf(el) === -1)?false:true   
+    }
   }
 }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.van-nav-bar
+
+
+.van-nav-bar{
+ background-color:#1E87F0;
+}
+.van-icon{
+  vertical-align: middle;
+}
 .wrap{
   background-color: #f9f9f9;
 }
 .wrap .grid-wrap{
   background-color: #fff;
-  margin-top: 2rem;
+  margin-top: 1rem;
+
 }
 .wrap .grid-wrap .grid-item{
   /*这6个值分别是：left值 、top值、透明度、阴影外延宽度、颜色、向里凹陷（选填，默认为外延）*/
@@ -156,6 +268,57 @@ export default {
   margin: 0.5rem 0;
   padding: 0.2rem 0.6rem;
   border-radius: 0.2rem;
+}
+
+.choose {
+  padding-left: 1.2rem;
+  height: 2.5rem;
+  line-height: 2.5rem;
+  text-align: left;
+  font-size: .86rem;
+  border-bottom:.05rem solid #f2f2f2;
+  color: rgb(160, 157, 157);
+}
+.choose span{
+  float: right;
+  margin-right: 1rem;
+  text-align: right;
+  font-size: 1.2rem;
+  color: rgb(24, 22, 22);
+}
+.choose1 {
+  height: 2.5rem;
+  padding-left:1.2rem;
+  line-height: 2.5rem;
+  text-align: left;
+  font-size: 1.2rem;
+  border-bottom:.05rem solid #f2f2f2;
+  color: #262626;
+}
+
+.group .item {
+  padding: .6rem;
+  padding-left: 1.8rem;
+  border-bottom: .05rem solid #f2f2f2;
+}
+.group :nth-child(1) {
+  padding-top:0;
+}
+.checkbox {
+  display: inline-block;
+  margin-right: .2rem;
+  vertical-align: middle;
+}
+.btn{
+  position: fixed;
+  z-index:1;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+.van-col{
+  padding: .7rem;
+  box-sizing: border-box;
 }
 
 </style>
