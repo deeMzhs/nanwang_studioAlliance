3fe4e478f3f60ec60fbf4c6964e4b94691e37f55
shusheng <863193681@qq.com>
2020-06-09 15:41:53 +0800

    'hss-创新培训页面更新'

--- src/pages/Innovate.vue
+++ src/pages/Innovate.vue
@@ -3,14 +3,19 @@
     <van-nav-bar title="Triz创新培训" fixed left-arrow @click-left="onClickLeft"></van-nav-bar>
     <div style="height: 46px"></div>
     <div class="innovate">
-      <div class="innovate_item" v-for="(item, index) in 5" :key="index">
-        <div class="innovate_item_left">
-          <img src="@/assets/img/photo.png" alt />
-          <div class="Classroom">第{{index+1}}课</div>
-        </div>
+      <div
+        class="innovate_item"
+        v-for="(item, index) in list"
+        :key="index"
+        @click="innovate(item.id)"
+      >
         <div class="innovate_item_right">
-            <div class="title">创新与TRIZ启蒙-创新方法（TRIZ） 理论及应用</div>
-            <div class="source">来源：哔哩哔哩</div>
+          <div class="title">{{item.title}}</div>
+          <div class="source">{{item.name}}</div>
+        </div>
+        <div class="innovate_item_left">
+          <img :src="item.imgUrl" alt />
+          <div class="Classroom">第{{convertToChinaNum(index+1)}}课</div>
         </div>
       </div>
     </div>
@@ -18,13 +23,109 @@
 </template>
 
 <script>
+import { Toast } from "vant";
 export default {
   data() {
-    return {};
+    return {
+      list: [
+        {
+          id: 1,
+          title: "创新与TRIZ启蒙-创新方法（TRIZ） 理论及应用",
+          imgUrl: require("@/assets/img/photo.jpg"),
+          name: "xxx工作室"
+        },
+        {
+          id: 2,
+          title: "创新与TRIZ启蒙-创新方法（TRIZ） 理论及应用",
+          imgUrl: require("@/assets/img/photo.jpg"),
+          name: "xxx工作室"
+        },
+        {
+          id: 3,
+          title: "创新与TRIZ启蒙-创新方法（TRIZ） 理论及应用",
+          imgUrl: require("@/assets/img/photo.jpg"),
+          name: "xxx工作室"
+        },
+        {
+          id: 4,
+          title: "创新与TRIZ启蒙-创新方法（TRIZ） 理论及应用",
+          imgUrl: require("@/assets/img/photo.jpg"),
+          name: "xxx工作室"
+        },
+        {
+          id: 5,
+          title: "创新与TRIZ启蒙-创新方法（TRIZ） 理论及应用",
+          imgUrl: require("@/assets/img/photo.jpg"),
+          name: "xxx工作室"
+        },
+        {
+          id: 6,
+          title: "创新与TRIZ启蒙-创新方法（TRIZ） 理论及应用",
+          imgUrl: require("@/assets/img/photo.jpg"),
+          name: "xxx工作室"
+        }
+      ]
+    };
   },
+
   methods: {
     onClickLeft() {
-      this.$router.push("/");
+      return this.$router.push("/");
+    },
+    innovate(id) {
+      Toast("id:" + id);
+    },
+    // 数字转中文
+    convertToChinaNum(num) {
+      let arr1 = new Array(
+        "零",
+        "一",
+        "二",
+        "三",
+        "四",
+        "五",
+        "六",
+        "七",
+        "八",
+        "九"
+      );
+      let arr2 = new Array(
+        "",
+        "十",
+        "百",
+        "千",
+        "万",
+        "十",
+        "百",
+        "千",
+        "亿",
+        "十",
+        "百",
+        "千",
+        "万",
+        "十",
+        "百",
+        "千",
+        "亿"
+      ); //可继续追加更高位转换值
+      if (!num || isNaN(num)) {
+        return "零";
+      }
+      let english = num.toString().split("");
+      let result = "";
+      for (let i = 0; i < english.length; i++) {
+        let des_i = english.length - 1 - i; //倒序排列设值
+        result = arr2[i] + result;
+        let arr1_index = english[des_i];
+        result = arr1[arr1_index] + result;
+      } //将【零千、零百】换成【零】 【十零】换成【十】
+      result = result.replace(/零(千|百|十)/g, "零").replace(/十零/g, "十"); //合并中间多个零为一个零
+      result = result.replace(/零+/g, "零"); //将【零亿】换成【亿】【零万】换成【万】
+      result = result.replace(/零亿/g, "亿").replace(/零万/g, "万"); //将【亿万】换成【亿】
+      result = result.replace(/亿万/g, "亿"); //移除末尾的零
+      result = result.replace(/零+$/, ""); //将【零一十】换成【零十】 //result = result.replace(/零一十/g, '零十');//貌似正规读法是零一十 //将【一十】换成【十】
+      result = result.replace(/^一十/g, "十");
+      return result;
     }
   }
 };
@@ -32,51 +133,51 @@ export default {
 
 <style scoped>
 .wrap {
-  background-color: #F8F8F8;
-  height:100vh;
+  background-color: #f8f8f8;
+  height: 100vh;
 }
 .innovate {
   background-color: #fff;
 }
 .innovate_item {
   box-sizing: border-box;
-  margin: .5rem 1rem;
-  padding: .5rem 0;
+  margin: 0.5rem 1rem;
+  padding: 0.5rem 0;
   display: flex;
-  border-bottom: .05rem solid rgb(238, 236, 236);
+  border-bottom: 0.05rem solid rgb(238, 236, 236);
 }
 .innovate_item_left {
   width: 30%;
   position: relative;
-  border-radius: .2rem;
+  border-radius: 0.2rem;
   overflow: hidden;
 }
-.innovate_item_left .Classroom{
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 4rem;
-    height: 1.5rem;
-    line-height: 1.5rem;
-    text-align: center;
-    color:#fff;
-    border-radius: .2rem;
-    overflow: hidden;
-    background-color: rgb(45, 148, 252);
+.innovate_item_left .Classroom {
+  position: absolute;
+  right: 0;
+  bottom: 0.2rem;
+  width: 4rem;
+  height: 1.5rem;
+  line-height: 1.5rem;
+  text-align: center;
+  color: #fff;
+  border-radius: 0.2rem;
+  overflow: hidden;
+  background-color: rgb(45, 148, 252);
 }
 
 .innovate_item_right {
-    flex: 1;
+  flex: 1;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  padding-left: .5rem;
+  padding-left: 0.5rem;
 }
-.innovate_item_right .title{
-  font-weight:bold;
-  font-size: .86rem;
+.innovate_item_right .title {
+  font-weight: 400;
+  font-size: 1rem;
 }
-.innovate_item_right .source{
-    color: #B2B2B2;
+.innovate_item_right .source {
+  color: #b2b2b2;
 }
 </style>
